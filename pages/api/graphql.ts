857422e389e849d76d076a17eeec3b87f3462ec3
Gareth Hind <garethjhind@gmail.com>
2022-02-02 10:45:35 +1100

    Setting up the GraphQL API route

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,18 +4,57 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-co
 import { NextApiHandler } from "next";
 
 const typeDefs = gql`
-  type Query {
-    users: [User!]!
+  enum TaskStatus {
+    active
+    completed
   }
-  type User {
-    name: String
+
+  type Task {
+    id: Int!
+    title: String!
+    status: TaskStatus!
+  }
+
+  input CreateTaskInput {
+    title: String!
+  }
+
+  input UpdateTaskInput {
+    id: Int!
+    title: String
+    status: TaskStatus
+  }
+
+  type Query {
+    tasks(status: TaskStatus): [Task!]!
+    task(id: Int!): Task
+  }
+
+  type Mutation {
+    createTask(input: CreateTaskInput!): Task
+    updateTask(input: UpdateTaskInput!): Task
+    deleteTask(id: Int!): Task
   }
 `;
 
 const resolvers: IResolvers = {
   Query: {
-    users(parent, args, context) {
-      return [{ name: "Nextjs" }];
+    tasks(parent, args, context) {
+      return [];
+    },
+    task(parent, args, context) {
+      return null;
+    },
+  },
+  Mutation: {
+    createTask(parent, args, context) {
+      return null;
+    },
+    updateTask(parent, args, context) {
+      return null;
+    },
+    deleteTask(parent, args, context) {
+      return null;
     },
   },
 };
